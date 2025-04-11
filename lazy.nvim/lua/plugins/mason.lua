62667f9fee85bc917410b85720544c9c95ee8876
essmpt1 <100933679+essmpt1@users.noreply.github.com>
2025-04-11 14:18:22 +0100

    Update mason.lua
    Com as confis para clang e intelephense

--- lazy.nvim/lua/plugins/mason.lua
+++ lazy.nvim/lua/plugins/mason.lua
@@ -10,9 +10,20 @@ return {
 		dependencies = { "mason.nvim" },
 		config = function()
 			require("mason-lspconfig").setup()
+
 			require("mason-lspconfig").setup_handlers({
 				function(server_name)
 					require("lspconfig")[server_name].setup({
+					})
+				end,
+				["clangd"] = function()
+					require("lspconfig").clangd.setup({
+						cmd = { "clangd" }, -- Caminho para o executável clangd
+						--document_formatting = false,
+						filetypes = { "c", "cpp", "objc", "objcpp", "h", "hpp" }, -- Tipos de arquivos suportados
+						root_dir = require("lspconfig").util.root_pattern("compile_commands.json", ".git"), -- Diretório raiz do projeto
+						single_file_support = true, -- Suporte para arquivos isolados
+						capabilities = require('cmp_nvim_lsp').default_capabilities(), -- Suporte para autocomplete avançado
 						on_attach = function(client, bufnr)
 							-- Ativa popups automáticas quando o cursor para em uma função
 							vim.api.nvim_create_autocmd("CursorHold", {
@@ -22,6 +33,32 @@ return {
 								end,
 							})
 						end
+					})
+				end,
+				['intelephense'] = function()
+					local lspconfig = require("lspconfig")
+					local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+					lspconfig.intelephense.setup({
+						cmd = { "intelephense", "--stdio" },
+						filetypes = { "php" },
+						capabilities = capabilities,
+						root_dir = lspconfig.util.root_pattern("composer.json", ".git", "."),
+						settings = {
+							intelephense = {
+								stubs = {
+									"bcmath", "bz2", "Core", "curl", "date", "dom",
+									"fileinfo", "filter", "gd", "hash", "iconv", "json",
+									"libxml", "mbstring", "openssl", "pcre", "PDO",
+									"pdo_mysql", "Phar", "readline", "Reflection", "session",
+									"SimpleXML", "sodium", "SPL", "standard", "tokenizer", "xml",
+									"xdebug", "zip", "zlib", "wordpress", "woocommerce"
+								},
+								files = {
+									maxSize = 1000000,
+								},
+							},
+						},
 					})
 				end
 			})
