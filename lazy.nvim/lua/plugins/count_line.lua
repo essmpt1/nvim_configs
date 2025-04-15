51748cba9ba9ff5e17865856ab97dfcd487e8cd2
Manuel Gouveia <malourei@student.42lisboa.com>
2025-04-16 00:09:24 +0100

    alteração para dar o delay

--- lazy.nvim/lua/plugins/count_line.lua
+++ lazy.nvim/lua/plugins/count_line.lua
@@ -3,13 +3,27 @@ return {
 	name = "ft_count_line",
 	lazy = false,
 	config = function()
-	  local f = require("ft_count_line")
+		local f = require("ft_count_line")
 
-	  vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "BufWinEnter" }, {
-		pattern = {"*.c", "*.cpp"},
-		callback = function()
-		  f.show_virtual_text()
-		end,
-	  })
+		local debounce_timer = nil
+
+		local function debounce_update()
+		if debounce_timer and not debounce_timer:is_closing() then
+			debounce_timer:stop()
+			debounce_timer:close()
+		end
+
+		debounce_timer = vim.defer_fn(function()
+			f.show_virtual_text()
+		end, 350) -- espera 350ms para evitar chamadas excessivas
+	end
+
+	vim.api.nvim_create_autocmd(
+		{ "BufWritePost", "BufEnter", "BufWinEnter", "TextChanged", "TextChangedI" },
+		{
+			pattern = { "*.c", "*.cpp" },
+			callback = debounce_update,
+		}
+	)
 	end,
-  }
+}
