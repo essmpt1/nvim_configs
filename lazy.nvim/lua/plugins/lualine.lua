aead6ce6b13aadd49c0680805c6526d7578aa411
essmpt1 <100933679+essmpt1@users.noreply.github.com>
2025-04-22 16:54:54 +0100

    Update lualine.lua
    Novas configs para o lualine

--- lazy.nvim/lua/plugins/lualine.lua
+++ lazy.nvim/lua/plugins/lualine.lua
@@ -1,3 +1,43 @@
+local function visual_selection_range()
+	local mode = vim.fn.mode()
+	if mode ~= 'v' and mode ~= 'V' and mode ~= '\22' then
+		return ''
+	end
+
+	local start_pos = vim.fn.getpos("v")
+	local end_pos = vim.fn.getpos(".")
+	local start_line = start_pos[2]
+	local start_col = start_pos[3]
+	local end_line = end_pos[2]
+	local end_col = end_pos[3]
+
+	-- Corrigir ordem se seleção foi "de baixo para cima"
+	if start_line > end_line or (start_line == end_line and start_col > end_col) then
+		start_line, end_line = end_line, start_line
+		start_col, end_col = end_col, start_col
+	end
+
+	local total_chars = 0
+
+	for lnum = start_line, end_line do
+		local line = vim.fn.getline(lnum)
+		local len = #line
+
+		if lnum == start_line and lnum == end_line then
+		-- Seleção dentro da mesma linha
+			total_chars = end_col - start_col + 1
+		elseif lnum == start_line then
+			total_chars = total_chars + (len - start_col + 2)
+		elseif lnum == end_line then
+			total_chars = total_chars + end_col
+		else
+			total_chars = total_chars + len + 1
+		end
+	end
+
+	return string.format("󰉺 %d chars", total_chars)
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -15,10 +55,15 @@ return {
 			lualine_a = { { "mode", icon = "" } },
 			lualine_b = { "branch", "diff", "diagnostics" },
 			lualine_c = { { "filename", path = 1 } }, -- mostra caminho relativo
-			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_x = { visual_selection_range, "encoding", "fileformat", "filetype" },
 			lualine_y = { "progress" },
-			lualine_z = { "location" }, -- igual ao %l:%c que usava no airline
+			lualine_z = { "location", {
+				function()
+					return " " .. os.date("%R")
+				  end,
+			} },
 		},
+		extension = { "lazy", "nvim-tree" },
 		tabline = {
 			lualine_a = {
 				{
